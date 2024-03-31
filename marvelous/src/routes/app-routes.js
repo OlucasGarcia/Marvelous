90d330cbf015c8327d4a375de461af058574e262
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2024-03-31 21:46:17 +0000

    T002: chore - Adicionado a rota da tela Details

--- marvelous/src/routes/app-routes.js
+++ marvelous/src/routes/app-routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../pages/home';
+import Details from '../pages/details';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,11 +10,15 @@ const AppRouter = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator
-            initialRouteName='Home'
+            initialRouteName='Details'
             >
             <Stack.Screen
             name='Home'
             component={Home}
+            />
+            <Stack.Screen
+            name='Details'
+            component={Details}
             />
         </Stack.Navigator>
         </NavigationContainer>
