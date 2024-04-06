cd9d16313b9723ed8e0c93b4f553b81a956d5f8b
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2024-04-06 06:42:37 +0000

    T002: feat - Finalização da estrutura e estilização básica da tela Home

--- marvelous/src/pages/home/index.js
+++ marvelous/src/pages/home/index.js
@@ -1,14 +1,60 @@
 import React from 'react-native';
 import styles from './styles';
-import { ScrollView } from 'react-native';
+import { ScrollView, View, Image } from 'react-native';
 import List from '../../components/list';
+import Icon from "react-native-vector-icons/AntDesign";
+import Logo from "../../../assets/marvelous_logo.png";
 
-const Home = () => {
+const Home = ({ navigation  }) => {
     return (
         <ScrollView
-        style={styles.container}
+            style={styles.container}
         >
-            <List/>
+            <View style={styles.nav}>
+                <Image
+                style={styles.logoImg}
+                source={Logo}
+                />
+                <Icon
+                style={styles.icons}
+                name='user'
+                size={35}
+                color="#000000"
+                />
+                <Icon
+                name='down'
+                size={15}
+                color="#000000"
+                />
+            </View>
+
+            <View style={styles.line}>
+                <p style={styles.txtNome}>
+                    Todos os lançamentos:
+                </p>
+                <List 
+                navigation={navigation}
+                />
+            </View>
+
+            <View style={styles.line}>
+                <p style={styles.txtNome}>
+                    Capitão América:
+                </p>
+                <List 
+                navigation={navigation}
+                />
+            </View>
+
+            <View style={styles.line}>
+                <p style={styles.txtNome}>
+                    Animações:
+                </p>
+                <List 
+                navigation={navigation}
+                />
+            </View>
+
         </ScrollView>
     );
 }
--- marvelous/src/pages/home/styles.js
+++ marvelous/src/pages/home/styles.js
@@ -3,9 +3,34 @@ import { StyleSheet } from "react-native";
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FAFAFA',
-        
+        backgroundColor: '#1E1E1E',
     },
+    nav: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        height: 60,
+    },
+    logoImg: {
+        width: 150,
+        height: 40,
+        marginHorizontal: 40,
+    },
+    icons: {
+        marginLeft: 70,
+    },
+    line: {
+        marginLeft: 15,
+    },
+    txtNome: {
+        color: 'white',
+        fontSize: 18,
+        fontFamily: 'arial',
+        textTransform: 'uppercase',
+        fontWeight: 600,
+        marginBottom: -5,
+    },
+
 });
 
 export default style;
